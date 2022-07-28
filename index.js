4eb6f8469f0637f9c5b326f156646780b9fe01f3
hasibul-99 <hasibul_uap@outlook.com>
2022-07-28 23:04:17 +0600

    242 solved

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-function romanToInt(s) {
-    var data = {
-        I: 1,
-        V: 5,
-        X: 10,
-        L: 50,
-        C: 100,
-        D: 500,
-        M: 1000
-    }, nData = {
-        IV: 4,
-        IX: 9,
-        XL: 40,
-        XC: 90,
-        CD: 400,
-        CM: 900
-    };
-    var total = 0, text = s;
-    Object.keys(nData).forEach(function (key) {
-        if (s.includes(key)) {
-            total += nData[key];
-            text = text.replace(key, "");
+function isAnagram(s, t) {
+    if ((s === null || s === void 0 ? void 0 : s.length) !== (t === null || t === void 0 ? void 0 : t.length))
+        return false;
+    var a = s.split("").sort(), b = t.split("").sort();
+    var result = true;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            result = false;
+            break;
         }
-    });
-    text.split("").forEach(function (n) {
-        total += data[n];
-    });
-    return total;
+    }
+    return result;
 }
 ;
-console.log(romanToInt("III"));
+console.log(isAnagram("anagran", "nagaram"));
