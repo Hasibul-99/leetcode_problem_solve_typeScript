f073e87963adfe35cff55e500779a3695d8b7efe
hasibul-99 <hasibul_uap@outlook.com>
2022-07-28 19:10:40 +0600

    13 solved

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function romanToInt(s: string): number {
-    let data = {
+function romanToInt(s) {
+    var data = {
         I: 1,
         V: 5,
         X: 10,
         L: 50,
         C: 100,
         D: 500,
-        M: 1000,
-    },
-    nData = {
+        M: 1000
+    }, nData = {
         IV: 4,
         IX: 9,
         XL: 40,
@@ -16,23 +15,17 @@ function romanToInt(s: string): number {
         CD: 400,
         CM: 900
     };
-
-    let total = 0,
-        text: string = s;
-
-    Object.keys(nData).forEach(function(key) {
-        if(s.includes(key)) {
+    var total = 0, text = s;
+    Object.keys(nData).forEach(function (key) {
+        if (s.includes(key)) {
             total += nData[key];
             text = text.replace(key, "");
         }
     });
-
-    text.split("").forEach(n => {
+    text.split("").forEach(function (n) {
         total += data[n];
-    })
-
+    });
     return total;
-
-};
-
+}
+;
 console.log(romanToInt("III"));
