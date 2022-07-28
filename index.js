4a8a5936ae08c0bf9bec98a3a5e81b920b5c2df2
hasibul-99 <hasibul_uap@outlook.com>
2022-07-28 23:20:25 +0600

    9 solved

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-function isAnagram(s, t) {
-    if ((s === null || s === void 0 ? void 0 : s.length) !== (t === null || t === void 0 ? void 0 : t.length))
+function isPalindrome(x) {
+    var a = x.toString(), re = a.split('').reverse().join("");
+    if (a === re)
+        return true;
+    else
         return false;
-    var a = s.split("").sort(), b = t.split("").sort();
-    var result = true;
-    for (var i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            result = false;
-            break;
-        }
-    }
-    return result;
 }
 ;
-console.log(isAnagram("anagran", "nagaram"));
+console.log(isPalindrome(101));
