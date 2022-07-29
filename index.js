04629e4b48f2d2b7128079d99e620c4a33ea4f20
hasibul-99 <hasibul_uap@outlook.com>
2022-07-29 12:23:00 +0600

    update

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-function isPalindrome(x) {
-    var a = x.toString(), re = a.split('').reverse().join("");
-    if (a === re)
-        return true;
-    else
-        return false;
+function longestCommonPrefix(strs) {
+    if (strs.length === 1)
+        return strs[0];
+    var result = "";
+    var ss = strs.reduce(function (a, b) { return a.length <= b.length ? a : b; });
+    strs.splice(strs.findIndex(function (s) { return s === ss; }), 1);
+    var text = ss;
+    while (text) {
+        for (var i = 0; i < strs.length; i++) {
+            if (!strs[i].startsWith(text)) {
+                text = text.slice(0, text.length - 1);
+                break;
+            }
+            if (i === strs.length - 1) {
+                result = text;
+                text = "";
+            }
+        }
+    }
+    return result;
 }
 ;
-console.log(isPalindrome(101));
+console.log("longestCommonPrefix", longestCommonPrefix(["reflower", "flow", "flight"]));
