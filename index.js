6ff048f5eec9ccaa06482279c2e9c86aa682f69e
hasibul-99 <hasibul_uap@outlook.com>
2022-07-31 18:16:34 +0600

    121 incomplete

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function mySqrt(x) {
-    var val = Math.sqrt(x);
-    return Math.floor(val);
+function maxProfit(prices) {
+    var sm = 0, max = 0;
+    for (var i = 0; i < prices.length; i++) {
+        if (prices[i] < prices[i + 1]) {
+            if ((!sm || !(sm < prices[i])) && prices[i] !== 0) {
+                sm = prices[i];
+            }
+        }
+    }
+    var index = prices.findIndex(function (e) { return e === sm; });
+    if (index !== -1) {
+        for (var j = index; j < prices.length; j++) {
+            if (!max || prices[j] > max) {
+                max = prices[j];
+            }
+        }
+    }
+    console.log("sm", sm);
+    console.log("max", max);
+    return max < sm ? 0 : max - sm;
 }
 ;
-console.log("removeNthFromEnd", mySqrt(8));
+console.log("maxProfit", maxProfit([3, 2, 6, 5, 0, 3]));
+// 121. Best Time to Buy and Sell Stock
--- index.ts
+++ index.ts
@@ -1,7 +1,34 @@
-function mySqrt(x: number): number {
-    let val = Math.sqrt(x);
+function maxProfit(prices: number[]): number {
+    let sm = 0,
+        max = 0;
 
-    return Math.floor(val)
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < prices[i + 1]) {
+            if ((!sm || !(sm < prices[i])) && prices[i] !== 0) {
+                sm = prices[i];
+            }
+        }
+    }
+
+    let index = prices.findIndex(e => e === sm);
+
+    if (index !== -1) {
+        for (let j = index; j < prices.length; j++) {
+            if (!max || prices[j] > max) {
+                max = prices[j]
+            }
+        }
+    }
+    
+
+    console.log("sm", sm);
+    console.log("max",max);
+    
+    
+
+    return max < sm ? 0 : max - sm ;
 };
 
-console.log("removeNthFromEnd", mySqrt(8));
+console.log("maxProfit", maxProfit([3,2,6,5,0,3]));
+
+// 121. Best Time to Buy and Sell Stock
