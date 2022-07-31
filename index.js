f4f6f89bde0377a071c7a7173e4ee46378df5665
hasibul-99 <hasibul_uap@outlook.com>
2022-07-31 09:31:00 +0600

    58

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 1)
-        return strs[0];
-    var result = "";
-    var ss = strs.reduce(function (a, b) { return a.length <= b.length ? a : b; });
-    strs.splice(strs.findIndex(function (s) { return s === ss; }), 1);
-    var text = ss;
-    while (text) {
-        for (var i = 0; i < strs.length; i++) {
-            if (!strs[i].startsWith(text)) {
-                text = text.slice(0, text.length - 1);
-                break;
-            }
-            if (i === strs.length - 1) {
-                result = text;
-                text = "";
-            }
-        }
-    }
-    return result;
+function lengthOfLastWord(s) {
+    var text = s.trim();
+    if (!text)
+        return 0;
+    var n = text.split(" ");
+    var lastWorld = n[n.length - 1];
+    return lastWorld.length;
 }
 ;
-console.log("longestCommonPrefix", longestCommonPrefix(["reflower", "flow", "flight"]));
+console.log("lengthOfLastWord", lengthOfLastWord("Hello World"));
