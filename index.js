06fc56121c783b0b27843d0f722917f6851de883
hasibul-99 <hasibul_uap@outlook.com>
2022-08-02 11:59:24 +0600

    20 not solved

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-function kthSmallest(matrix, k) {
-    var arr = matrix.reduce(function (r, e) { return (r.push.apply(r, e), r); }, []);
-    arr.sort(function (a, b) { return a - b; });
-    return arr[k - 1];
+function isValid(s) {
+    var data = [
+        { s: '(', n: ')' },
+        { s: '{', n: '}' },
+        { s: '[', n: ']' }
+    ];
+    var str = s.split(''), result = true;
+    var _loop_1 = function (i) {
+        if ((i + 1) % 2 === 0) {
+            var s_1 = str[i - 1], n_1 = str[i];
+            var findIdx = data.findIndex(function (item) { return item.s === s_1 && item.n === n_1; });
+            if (findIdx === -1) {
+                result = false;
+                return "break";
+            }
+        }
+    };
+    for (var i = 0; i < str.length; i++) {
+        var state_1 = _loop_1(i);
+        if (state_1 === "break")
+            break;
+    }
+    return result;
 }
 ;
-console.log("kthSmallest", kthSmallest([[1, 5, 9], [10, 11, 13], [12, 13, 15]], 8));
+console.log("kthSmallest", isValid("(]"));
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/submissions/
--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
-function kthSmallest(matrix: number[][], k: number): number {
-    let arr = matrix.reduce((r, e) => (r.push(...e), r), []);
+function isValid(s: string): boolean {
+    let data = [
+        {s: '(', n: ')'},
+        {s: '{', n: '}'},
+        {s: '[', n: ']'}
+    ];
 
-    arr.sort(function(a, b){return a - b});
+    let str = s.split(''),
+        result = true;
 
-    return arr[k - 1];
+    for (let i = 0; i < str.length; i++) {
+        if ((i+1)% 2 === 0 ) {
+            let s = str[i -1],
+                n = str[i];
+                
+            let findIdx = data.findIndex(item => item.s === s && item.n === n);
+
+            if (findIdx === -1) {
+                result = false;
+                break;
+            }
+        }
+    }
+    
+    return result;
 };
 
 
-console.log("kthSmallest", kthSmallest([[1,5,9],[10,11,13],[12,13,15]], 8));
+console.log("kthSmallest", isValid("(]"));
+
+// 20. Valid Parentheses
+// https://leetcode.com/problems/valid-parentheses/submissions/
