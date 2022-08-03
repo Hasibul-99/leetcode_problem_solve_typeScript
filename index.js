90b2b850e6d2a32129a70c8a95bbc6d9705940c9
hasibul-99 <hasibul_uap@outlook.com>
2022-08-03 18:19:26 +0600

    49 not solved

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-function strStr(haystack, needle) {
-    if (haystack.includes(needle)) {
-        var arr = haystack.split(needle), f = arr[0];
-        return f.length;
+function groupAnagrams(strs) {
+    var arr = [], items = strs.sort(function (a, b) { return a.length - b.length; });
+    var _loop_1 = function (index) {
+        var item = sortAlphabets(strs[index]);
+        var filter = items.filter(function (n) { return sortAlphabets(n) === item; });
+        var _loop_2 = function (j) {
+            var element = filter[j];
+            var index_1 = strs.findIndex(function (s) { return s === element; });
+            if (index_1 !== -1) {
+                items.splice(index_1, 1);
+            }
+        };
+        for (var j = 0; j < filter.length; j++) {
+            _loop_2(j);
+        }
+        arr.push(filter);
+    };
+    for (var index = 0; index < strs.length; index++) {
+        _loop_1(index);
+    }
+    // return items.length ? arr.concat([items]) : arr;
+    if (items.length) {
+        items.forEach(function (n) {
+            arr.push([n]);
+        });
+        return arr.sort(function (a, b) { return a.length - b.length; });
     }
     else
-        return -1;
+        return arr.sort(function (a, b) { return a.length - b.length; });
 }
 ;
-console.log("strStr", strStr("umbrella", "laa"));
+var sortAlphabets = function (text) {
+    return text.split('').sort().join('');
+};
+console.log("strStr", groupAnagrams(["stop", "pots", "reed", "", "tops", "deer", "opts", ""]));
+// 49. Group Anagrams
