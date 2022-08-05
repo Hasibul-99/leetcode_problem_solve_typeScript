ecbf3ca45a7994ab9ec72bbce437a8c906760457
hasibul-99 <hasibul_uap@outlook.com>
2022-08-05 16:39:31 +0600

    myatoi

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-function countBits(n) {
-    var arr = [];
-    var _loop_1 = function (i) {
-        var bit = Number(i).toString(2);
-        var sp = bit.split('');
-        var total = 0;
-        sp.forEach(function (n) {
-            total += parseInt(n);
-        });
-        arr.push(total);
-    };
-    for (var i = 0; i <= n; i++) {
-        _loop_1(i);
+function myAtoi(s) {
+    var num = parseInt(s);
+    if (Math.pow(2, 31) <= num) {
+        return Math.pow(2, 31) - 1;
     }
-    return arr;
+    else if (Math.pow(-2, 31) > num) {
+        return Math.pow(-2, 31);
+    }
+    else {
+        return num ? num : 0;
+    }
 }
 ;
-console.log("removeElement", countBits(5));
-// https://leetcode.com/problems/counting-bits/
+console.log("myAtoi", myAtoi("2147483648"));
