253c4c232d845617c0ebe815dfb8568dd59978c4
hasibul-99 <hasibul_uap@outlook.com>
2022-08-07 16:23:12 +0600

    67 not solved

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-function repeatedCharacter(s) {
-    if (s.length === 1)
-        return s;
-    var result = '';
-    var arr = [];
-    var _loop_1 = function (i) {
-        var index = arr.findIndex(function (e) { return e === s[i]; });
-        if (index === -1) {
-            arr.push(s[i]);
-        }
-        else {
-            result = s[i];
-            return "break";
-        }
-    };
-    for (var i = 0; i < s.length; i++) {
-        var state_1 = _loop_1(i);
-        if (state_1 === "break")
-            break;
-    }
-    return result;
+function addBinary(a, b) {
+    var x = parseInt(a, 2);
+    var y = parseInt(b, 2);
+    console.log("x", x);
+    console.log("y", y);
+    var z = x + y;
+    console.log("z", z);
+    return z.toString(2);
 }
 ;
-console.log("repeatedCharacter", repeatedCharacter("abcdd"));
+console.log("repeatedCharacter", addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
