fa71d2a20be2ba67cb9456c244bc66e5a8f90ff6
hasibul-99 <hasibul_uap@outlook.com>
2022-08-08 12:15:11 +0600

    136 solved

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function addBinary(a, b) {
-    var x = parseInt(a, 2);
-    var y = parseInt(b, 2);
-    console.log("x", x);
-    console.log("y", y);
-    var z = x + y;
-    console.log("z", z);
-    return z.toString(2);
+function singleNumber(nums) {
+    var arr = [], result = nums[0];
+    var _loop_1 = function (i) {
+        var ele = nums[i], l = nums.filter(function (n) { return n === ele; });
+        var data = {
+            val: ele,
+            len: l.length
+        };
+        arr.push(data);
+    };
+    for (var i = 0; i < nums.length; i++) {
+        _loop_1(i);
+    }
+    ;
+    var fin = arr.find(function (a) { return a.len === 1; });
+    return fin.val;
 }
 ;
-console.log("repeatedCharacter", addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
+console.log("singleNumber", singleNumber([4, 1, 2, 1, 2]));
