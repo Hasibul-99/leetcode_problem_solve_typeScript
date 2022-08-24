06f99ff6087a5510d6ce48b581dfb5757f089056
hasibul-99 <hasibul_uap@outlook.com>
2022-08-24 14:04:51 +0600

    update

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-<<<<<<< HEAD
-function fib(n) {
+function isPowerOfThree(n) {
+    if (n === 1) {
+        return true;
+    }
+    else if (n > 1 && n % 3 === 0) {
+        return isPowerOfThree(n / 3);
+    }
+    else {
+        return false;
+    }
 }
 ;
-=======
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {number[]} nums
- * @return {TreeNode}
- */
-
-
-var sortedArrayToBST = function(nums, left = 0, right = nums.length -1) {
-    if (left > right) return null;
-
-    let mid = Math.floor((left + right) / 2);
-    let root = new TreeNode(nums[mid]);
-    
-    console.log(root)
-
-    root.left = sortedArrayToBST(nums, left, mid - 1);
-    root.right = sortedArrayToBST(nums, mid + 1, right);
-
-    return root;
-};
-
-console.log(sortedArrayToBST([-10,-3,0,5,9]));
->>>>>>> ffe279716ead820589240287be886658cd19afc3
+console.log("isPowerOfThree", isPowerOfThree(0));
