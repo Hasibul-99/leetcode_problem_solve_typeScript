efa1bcdd4252c12cd0b8c55ebff9698662e28a70
hasibul-99 <hasibul_uap@outlook.com>
2022-08-31 00:06:04 +0600

    724 not solved

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function runningSum(nums) {
-    var result = [];
+function pivotIndex(nums) {
+    var index = -1;
     for (var i = 0; i < nums.length; i++) {
-        var val = i === 0 ? nums[i] : nums[i] + result[i - 1];
-        result.push(val);
+        var left = nums.slice(0, i);
+        var right = nums.slice(i + 1);
+        console.log("arr2.splice(0,1)", { left: left, right: right });
+        var leftTotal = (left === null || left === void 0 ? void 0 : left.length) ? left.reduce(function (total, val) { return (total || 0) + val; }) : 0;
+        var rightTotal = (right === null || right === void 0 ? void 0 : right.length) ? right.reduce(function (total, val) { return (total || 0) + val; }) : 0;
+        console.log({ leftTotal: leftTotal, rightTotal: rightTotal });
+        if (leftTotal === rightTotal) {
+            index = i;
+            break;
+        }
     }
-    return result;
+    return index;
 }
 ;
-console.log("runningSum", runningSum([3, 1, 2, 10, 1]));
+console.log("pirvotIndex", pivotIndex([2, 1, -1]));
--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
-function runningSum(nums: number[]): number[] {
-    let result: number[] = [];
+function pivotIndex(nums: number[]): number {
+    let index = -1;
 
     for (let i = 0; i < nums.length; i++) {
-        let val = i=== 0 ? nums[i] : nums[i] + result[i -1];
-        result.push(val);
+        let left = nums.slice(0, i);
+        let right = nums.slice(i + 1);
+
+        console.log("arr2.splice(0,1)", {left, right});
+
+        let leftTotal = left?.length ? left.reduce((total, val) => (total || 0) + val) : 0;
+        let rightTotal = right?.length ? right.reduce((total, val) => (total || 0) + val) : 0;
+
+        console.log({leftTotal, rightTotal});
+
+        if (leftTotal === rightTotal) {
+            index = i;
+            break;
+        }
     }
 
-    return result;
+    return index;
 };
 
-console.log("runningSum", runningSum([3,1,2,10,1]));
+console.log("pirvotIndex", pivotIndex([2,1,-1]));
