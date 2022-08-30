ee0cd31905771d7882ad9872acc8d88dcb8dd2aa
hasibul-99 <hasibul_uap@outlook.com>
2022-08-30 10:30:55 +0600

    1480 update

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
-<<<<<<< HEAD
-function fib(n) {
+function runningSum(nums) {
+    var result = [];
+    for (var i = 0; i < nums.length; i++) {
+        var val = i === 0 ? nums[i] : nums[i] + result[i - 1];
+        result.push(val);
+    }
+    return result;
 }
 ;
-=======
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {number[]} nums
- * @return {TreeNode}
- */
-
-
-var sortedArrayToBST = function(nums, left = 0, right = nums.length -1) {
-    if (left > right) return null;
-
-    let mid = Math.floor((left + right) / 2);
-    let root = new TreeNode(nums[mid]);
-    
-    console.log(root)
-
-    root.left = sortedArrayToBST(nums, left, mid - 1);
-    root.right = sortedArrayToBST(nums, mid + 1, right);
-
-    return root;
-};
-
-console.log(sortedArrayToBST([-10,-3,0,5,9]));
->>>>>>> ffe279716ead820589240287be886658cd19afc3
+console.log("runningSum", runningSum([3, 1, 2, 10, 1]));
--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
-function sum(num1: number, num2: number): number {
-    return num1 + num2;
+function runningSum(nums: number[]): number[] {
+    let result: number[] = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let val = i=== 0 ? nums[i] : nums[i] + result[i -1];
+        result.push(val);
+    }
+
+    return result;
 };
 
-console.log("sum", sum(10, 6));
+console.log("runningSum", runningSum([3,1,2,10,1]));
