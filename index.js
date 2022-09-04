8f8f218e9ff26df35634278b14ea825fc2618965
hasibul-99 <hasibul_uap@outlook.com>
2022-09-04 14:21:01 +0600

    21 solved

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-function isSubsequence(s, t) {
-    if (!s.length)
-        return true;
-    var point = 0;
-    for (var i = 0; i < t.length; i++) {
-        if (s[point] === t[i])
-            point++;
+var mergeTwoLists = function(l1, l2) {
+    if (l1 === null) {
+        return l2;
+    } 
+    else if (l2 === null) {
+        return l1;
     }
-    return point === s.length;
-}
-;
-console.log("isSubsequence", isSubsequence("abc", "ahbgdc"));
+    else if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
+    }
+};
+
+console.log("mergeTwoLists", mergeTwoLists([1,2,4], [1,3,4]));
