b41e9de4b382e87a6b17dbe77de1bd5025569a0a
hasibul-99 <hasibul_uap@outlook.com>
2022-09-07 12:07:52 +0600

    409 slved

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-var mergeTwoLists = function(l1, l2) {
-    if (l1 === null) {
-        return l2;
-    } 
-    else if (l2 === null) {
-        return l1;
+function longestPalindrome(s) {
+    if (!(s === null || s === void 0 ? void 0 : s.length))
+        return 0;
+    var counts = {}, total = 0, odds = [];
+    for (var i = 0; i < s.length; i++) {
+        var ele = s[i];
+        if (counts[ele]) {
+            counts[ele] += 1;
+        }
+        else {
+            counts[ele] = 1;
+        }
     }
-    else if (l1.val < l2.val) {
-        l1.next = mergeTwoLists(l1.next, l2);
-        return l1;
-    } else {
-        l2.next = mergeTwoLists(l1, l2.next);
-        return l2
+    ;
+    console.log("counts", counts);
+    for (var _i = 0, _a = Object.keys(counts); _i < _a.length; _i++) {
+        var key = _a[_i];
+        if ((counts[key] % 2) === 0) {
+            total = total + (counts[key]);
+        }
+        else {
+            var data = {
+                key: key,
+                val: counts[key]
+            };
+            odds.push(counts[key]);
+        }
     }
-};
-
-console.log("mergeTwoLists", mergeTwoLists([1,2,4], [1,3,4]));
+    console.log("oddCounts", odds);
+    var maximum = Math.max.apply(Math, odds);
+    var minimum = Math.min.apply(Math, odds);
+    console.log("maximum", maximum);
+    console.log("minimum", minimum);
+    var counter = true;
+    odds.forEach(function (odd) {
+        if (minimum === 1) {
+            if (odd === 1) {
+                if (counter) {
+                    total = total + 1;
+                    counter = false;
+                }
+            }
+            else {
+                total = total + (odd - 1);
+            }
+        }
+        else {
+            if (odd === maximum && counter) {
+                total = total + maximum;
+                counter = false;
+            }
+            else {
+                total = total + (odd - 1);
+            }
+        }
+    });
+    return total;
+}
+;
+console.log("longestPalindrome", longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
