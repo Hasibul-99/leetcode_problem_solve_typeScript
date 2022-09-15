2d5002b2d3e1ef7a1367efaa92f678c6470f2e24
hasibul-99 <hasibul_uap@outlook.com>
2022-09-15 09:49:08 +0600

    1909 solved

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-function validMountainArray(arr) {
-    var result = true;
-    var max = Math.max.apply(Math, arr), index = arr.findIndex(function (a) { return a === max; });
-    if (index === 0)
-        return false;
-    if (index === arr.length - 1)
-        return false;
-    // console.log(max);
-    // ====>
-    for (var i = index; i < arr.length; i++) {
-        var ele = arr[i], next = arr[i + 1];
-        if (next && ele <= next) {
-            console.log({ ele: ele, next: next });
-            result = false;
-            break;
+function canBeIncreasing(nums) {
+    var count = 0;
+    for (var i = 1; i < nums.length; i++) {
+        if (nums[i - 1] >= nums[i]) {
+            count += 1;
+            if (i > 1 && nums[i - 2] >= nums[i]) {
+                nums[i] = nums[i - 1];
+            }
         }
     }
-    // <====
-    for (var j = index; j >= 0; j--) {
-        var ele = arr[j], pre = arr[j - 1];
-        // console.log("ele2", ele);
-        if (pre && ele <= pre) {
-            console.log({ ele: ele, pre: pre });
-            result = false;
-            break;
-        }
-    }
-    return result;
+    return count <= 1 ? true : false;
 }
 ;
-console.log("validMountainArray", validMountainArray([1, 3, 2]));
+console.log("canBeIncreasing", canBeIncreasing([6, 7, 2, 8, 9]));
