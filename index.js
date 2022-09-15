461f484432d05286c18663925e230e16cf717d4f
hasibul-99 <hasibul_uap@outlook.com>
2022-09-15 13:10:41 +0600

    2114 solved

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-// 2007. Find Original Array From Doubled Array JavaScript / typeScript 
-function findOriginalArray(changed) {
-    if ((changed.length % 2) !== 0)
-        return [];
-    var result = [], q = [];
-    changed.sort(function (a, b) { return a - b; });
-    changed.forEach(function (num) {
-        if (q.length && num === q[0]) {
-            q.shift();
-        }
-        else {
-            q.push(num * 2);
-            result.push(num);
+function mostWordsFound(sentences) {
+    var result = 0;
+    sentences.forEach(function (sen) {
+        console.log(sen);
+        if (sen) {
+            console.log("ere", sen.split(" ").length);
+            var val = sen.split(" ").length;
+            if (val > result)
+                result = val;
         }
     });
-    console.log("result", result);
-    console.log("q", q);
-    return q.length ? [] : result;
+    return result;
 }
 ;
-console.log("findOriginalArray", findOriginalArray([6, 3, 0, 1]));
+console.log("mostWordsFound", mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]));
