d5e94093635df1537fb33c244a28d93f9773014e
hasibul-99 <hasibul_uap@outlook.com>
2022-09-22 16:09:17 +0600

    557 solved

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-function mostWordsFound(sentences) {
-    var result = 0;
-    sentences.forEach(function (sen) {
-        console.log(sen);
-        if (sen) {
-            console.log("ere", sen.split(" ").length);
-            var val = sen.split(" ").length;
-            if (val > result)
-                result = val;
-        }
+function reverseWords(s) {
+    var arr = s.split(' ');
+    var text = '';
+    arr.forEach(function (a, i) {
+        text = text + a.split("").reverse().join("") + "".concat(i === arr.length - 1 ? '' : ' ');
     });
-    return result;
+    return text;
 }
 ;
-console.log("mostWordsFound", mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]));
+console.log("reverseWords", reverseWords("Let's take LeetCode contest"));
