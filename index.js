ea019601e9efa6615fb9175476621b62e263bc17
hasibul-99 <hasibul_uap@outlook.com>
2022-09-26 12:16:17 +0600

    4. Median of Two Sorted Arrays solved

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function reverseWords(s) {
-    var arr = s.split(' ');
-    var text = '';
-    arr.forEach(function (a, i) {
-        text = text + a.split("").reverse().join("") + "".concat(i === arr.length - 1 ? '' : ' ');
-    });
-    return text;
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function findMedianSortedArrays(nums1, nums2) {
+    var items = __spreadArray(__spreadArray([], nums1, true), nums2, true).sort(function (a, b) { return a - b; });
+    console.log("items", items);
+    if (items.length % 2 === 0) {
+        var floor = items[Math.floor(items.length / 2) - 1], cling = items[Math.round(items.length / 2)];
+        return (floor + cling) / 2;
+    }
+    else {
+        return items[Math.floor(items.length / 2)];
+    }
 }
 ;
-console.log("reverseWords", reverseWords("Let's take LeetCode contest"));
+console.log("findMedianSortedArrays", findMedianSortedArrays([1, 2], [3, 4]));
