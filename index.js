6b36313882cc953e45dbfac03210cc29a37624ae
hasibul-99 <hasibul_uap@outlook.com>
2022-12-18 15:11:22 +0600

    739. Daily Temperatures

--- index.js
+++ index.js
@@ -1,33 +1,16 @@
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
+function dailyTemperatures(temperatures) {
+    var arr = Array(temperatures.length).fill(0);
+    for (var i = 0; i < temperatures.length; i++) {
+        // console.log(temperatures[i]);
+        for (var j = i + 1; j < temperatures.length; j++) {
+            console.log({ i: temperatures[i], j: temperatures[j] });
+            if (temperatures[j] > temperatures[i]) {
+                arr[i] = j - i;
+                break;
+            }
         }
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-function minFallingPathSum(matrix) {
-    // const m = matrix.length,
-    //     n = matrix[0].length,
-    //     dp = Array(n).fill(0);
-    // console.log("dp",dp);
-    // for (let i = 0; i < m; i++) {
-    //     let prev = [...dp]
-    //     for (let j = 0; j < n; j++) {
-    //         dp[j] = Math.min(prev[j - 1] || Infinity, prev[j], prev[j + 1] || Infinity) +
-    //             matrix[i][j]
-    //     }
-    // }
-    // return Math.min(...dp)
-    var dp = Array(matrix[0].length).fill(0);
-    for (var i = 0; i < matrix.length; i++) {
-        var prev = __spreadArray([], dp, true);
-        for (var j = 0; j < matrix[0].length; j++) {
-            dp[j] = Math.min(prev[j - 1] || Infinity, prev[j], prev[j + 1] || Infinity) + matrix[i][j];
-        }
-    }
-    return Math.min.apply(Math, dp);
+    return arr;
 }
 ;
-console.log("minFallingPathSum", minFallingPathSum([[100, -42, -46, -41], [31, 97, 10, -10], [-58, -51, 82, 89], [51, 81, 69, -51]]));
+console.log("dailyTemperatures", dailyTemperatures([30, 60, 90]));
