c479b52ef9122d3aa84331c53e52778ec68984cd
hasibul-99 <hasibul_uap@outlook.com>
2023-01-02 14:58:10 +0600

    520. Detect Capital

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-function wordPattern(pattern, s) {
-    var a = pattern.split(''), b = s.split(' '), pt = [], x = {};
-    if (a.length !== b.length)
+function detectCapitalUse(word) {
+    var len = word.length;
+    if (len === 0)
         return false;
-    for (var i = 0; i < a.length; i++) {
-        if (!x[a[i]]) {
-            x[a[i]] = b[i];
-            if (pt.indexOf(b[i]) !== -1)
-                return false;
-            pt.push(b[i]);
-        }
-        else if (x[a[i]] !== b[i])
-            return false;
-    }
-    return true;
+    return word.toUpperCase() === word || word.slice(1).toLowerCase() === word.slice(1);
 }
 ;
-console.log("wordPattern", wordPattern("abba", "dog cat cat dog"));
+console.log("detectCapitalUse", detectCapitalUse("FlaG"));
