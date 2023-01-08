537bf28774bdc7fdf02158af4a3b883f386f7ae6
hasibul-99 <hasibul_uap@outlook.com>
2023-01-08 12:24:21 +0600

    149. Max Points on a Line

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-function canCompleteCircuit(gas, cost) {
-    var lan = gas.length, tank = 0, total = 0, start = 0;
-    for (var i = 0; i < lan; i++) {
-        var g = gas[i], c = cost[i];
-        tank += (g - c);
-        if (tank < 0) {
-            start = i + 1;
-            total += tank;
-            tank = 0;
+function maxPoints(points) {
+    var len = points.length, max = 2;
+    if (len <= 2)
+        return len;
+    for (var i = 0; i < len; i++) {
+        for (var j = i + 1; j < len; j++) {
+            var total = 2;
+            for (var k = 0; k < len; k++) {
+                if (k !== i && k !== j) {
+                    // d = ((y2 -y1)/(x2 -x1)) * ((y3 -y1) /(x3 - x1));
+                    if (((points[j][1] - points[i][1]) * (points[i][0] - points[k][0])) === ((points[i][1] - points[k][1]) * (points[j][0] - points[i][0]))) {
+                        total = total + 1;
+                    }
+                }
+            }
+            console.log({ max: max, total: total });
+            max = Math.max(max, total);
         }
     }
-    ;
-    return tank + total >= 0 ? start : -1;
+    return max;
 }
 ;
-console.log("canCompleteCircuit", canCompleteCircuit([5, 1, 2, 3, 4], [4, 4, 1, 5, 1]));
+console.log("maxPoints", maxPoints([[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]));
