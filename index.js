d4bb2d76a11b7414ed2313482bc29ba86b978e37
hasibul-99 <hasibul_uap@outlook.com>
2023-01-09 17:20:16 +0600

    144. Binary Tree Preorder Traversal

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-function maxPoints(points) {
-    var len = points.length, max = 2;
-    if (len <= 2)
-        return len;
-    for (var i = 0; i < len; i++) {
-        for (var j = i + 1; j < len; j++) {
-            var total = 2;
-            for (var k = 0; k < len; k++) {
-                if (k !== i && k !== j) {
-                    // d = ((y2 -y1)/(x2 -x1)) * ((y3 -y1) /(x3 - x1));
-                    if (((points[j][1] - points[i][1]) * (points[i][0] - points[k][0])) === ((points[i][1] - points[k][1]) * (points[j][0] - points[i][0]))) {
-                        total = total + 1;
-                    }
-                }
-            }
-            console.log({ max: max, total: total });
-            max = Math.max(max, total);
-        }
-    }
-    return max;
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function preorderTraversal(root) {
+    var walk = function (node, result) {
+        if (result === void 0) { result = []; }
+        if (!node)
+            return result;
+        result.push(node.val);
+        walk(node.left, result);
+        walk(node.right, result);
+        return result;
+    };
+    return walk(root, []);
 }
 ;
-console.log("maxPoints", maxPoints([[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]));
+console.log("preorderTraversal", preorderTraversal([1, null, 2, 3]));
