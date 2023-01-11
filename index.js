9b9772d3128b25808c18ed42fc87a47d63b6425e
hasibul-99 <hasibul_uap@outlook.com>
2023-01-11 23:27:34 +0600

    1443. Minimum Time to Collect All Apples in a Tree

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-function preorderTraversal(root) {
-    var walk = function (node, result) {
-        if (result === void 0) { result = []; }
-        if (!node)
-            return result;
-        result.push(node.val);
-        walk(node.left, result);
-        walk(node.right, result);
-        return result;
+function minTime(n, edges, hasApple) {
+    var child = new Array(n);
+    for (var i = 0; i < n; i++) {
+        child[i] = new Array();
+    }
+    ;
+    for (var _i = 0, edges_1 = edges; _i < edges_1.length; _i++) {
+        var edge = edges_1[_i];
+        child[edge[0]].push(edge[1]);
+        child[edge[1]].push(edge[0]);
+    }
+    ;
+    console.log("child", child);
+    var res = 0;
+    var dfs = function (node, parent) {
+        var val = false;
+        console.log("child[node]", child[node]);
+        for (var _i = 0, _a = child[node]; _i < _a.length; _i++) {
+            var c = _a[_i];
+            if (c === parent)
+                continue;
+            res++;
+            var bol = dfs(c, node);
+            if (bol)
+                res++;
+            else
+                res--;
+            val = val || bol;
+        }
+        if (hasApple[node])
+            return true;
+        return val;
     };
-    return walk(root, []);
+    dfs(0, null);
+    return res;
 }
-;
-console.log("preorderTraversal", preorderTraversal([1, null, 2, 3]));
+console.log("minTime", minTime(7, [[0, 1], [0, 2], [1, 4], [1, 5], [2, 3], [2, 6]], [false, false, true, false, true, true, false]));
