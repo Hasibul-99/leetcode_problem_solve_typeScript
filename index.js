2c4d6f88245acfb9c2d587b7c9d3ccd2f4be4bcd
hasibul-99 <hasibul_uap@outlook.com>
2023-01-12 14:32:49 +0600

    2529. Maximum Count of Positive Integer and Negative Integer

--- index.js
+++ index.js
@@ -1,34 +1,13 @@
-function countSubTrees(n, edges, labels) {
-    var adj = [], countMap = {}, result = [];
-    for (var i = 0; i < n; i++) {
-        adj.push([]);
-        result.push(0);
-        countMap[labels[i]] = 0;
+function maximumCount(nums) {
+    var p = 0, n = 0;
+    for (var i = 0; i < nums.length; i++) {
+        var e = nums[i];
+        if (e > 0)
+            p = p + 1;
+        if (e < 0)
+            n = n + 1;
     }
-    ;
-    for (var j = 0; j < edges.length; j++) {
-        var edge = edges[j];
-        adj[edge[0]].push(edge[1]);
-        adj[edge[1]].push(edge[0]);
-    }
-    ;
-    var visited = {};
-    var dfs = function (node) {
-        var label = labels[node];
-        console.log(label);
-        if (visited[node])
-            return false;
-        visited[node] = true;
-        var start = countMap[label];
-        for (var _i = 0, _a = adj[node]; _i < _a.length; _i++) {
-            var next = _a[_i];
-            dfs(next);
-        }
-        countMap[label] = 1 + countMap[label];
-        result[node] = countMap[label] - start;
-    };
-    dfs(0);
-    return result;
+    return Math.max(p, n);
 }
 ;
-console.log("countSubTrees", countSubTrees(4, [[0, 2], [0, 3], [1, 2]], "aeed"));
+console.log("maximumCount", maximumCount([-2, -1, -1, 1, 2, 3]));
