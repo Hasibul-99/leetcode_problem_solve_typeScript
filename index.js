cc1ec2654587885421b93460c2331199979263f1
hasibul-99 <hasibul_uap@outlook.com>
2023-01-14 01:36:18 +0600

    2246. Longest Path With Different Adjacent Characters

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-function maximumCount(nums) {
-    var p = 0, n = 0;
-    for (var i = 0; i < nums.length; i++) {
-        var e = nums[i];
-        if (e > 0)
-            p = p + 1;
-        if (e < 0)
-            n = n + 1;
+function longestPath(parent, s) {
+    var len = parent.length, result = 0, graph = parent.map(function (p) { return []; });
+    for (var i = 1; i < len; i++) {
+        graph[parent[i]].push(i);
     }
-    return Math.max(p, n);
+    ;
+    var longestPathDownFrom = function (node) {
+        var max1 = 0, max2 = 0;
+        for (var _i = 0, _a = graph[node]; _i < _a.length; _i++) {
+            var sNode = _a[_i];
+            var res = longestPathDownFrom(sNode);
+            if (s[node] === s[sNode])
+                continue;
+            if (res > max1) {
+                max2 = max1,
+                    max1 = res;
+            }
+            else if (res > max2) {
+                max2 = res;
+            }
+        }
+        ;
+        result = Math.max(result, 1 + max1 + max2);
+        return 1 + max1;
+    };
+    longestPathDownFrom(0);
+    return result;
 }
 ;
-console.log("maximumCount", maximumCount([-2, -1, -1, 1, 2, 3]));
+console.log("longestPath", longestPath([-1, 0, 0, 0], "aabc"));
