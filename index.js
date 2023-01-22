12005ea680483d2f00691108affdb6c8524a5e3d
hasibul-99 <hasibul_uap@outlook.com>
2023-01-22 22:20:47 +0600

    131. Palindrome Partitioning

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-function alternateDigitSum(n) {
-    var s = n.toString().split(""), res = 0;
-    for (var i = 0; i < s.length; i++) {
-        var ele = parseInt(s[i]);
-        if (i % 2 === 0) {
-            res = res + ele;
+function partition(s) {
+    var getDp = function (s) {
+        var len = s.length, dp = Array(len);
+        for (var i = 0; i < len; i++) {
+            for (var j = 0; j <= i; j++) {
+                if (!dp[j])
+                    dp[j] = Array(len);
+                dp[j][i] = (s[i] === s[j]) && (i - j <= 2 || dp[j + 1][i - 1]);
+            }
         }
-        else {
-            res = res + (-ele);
+        return dp;
+    };
+    var dfs = function (dp, res, now, s, index) {
+        var len = s.length;
+        if (index === len) {
+            res.push(Array.from(now));
+            return;
         }
-    }
+        for (var i = index; i < len; i++) {
+            if (dp[index][i]) {
+                now.push(s.substring(index, i + 1));
+                dfs(dp, res, now, s, i + 1);
+                now.pop();
+            }
+        }
+    };
+    var res = [], now = [];
+    var dp = getDp(s);
+    dfs(dp, res, now, s, 0);
+    console.log("dp", dp);
     return res;
 }
 ;
-console.log("alternateDigitSum", alternateDigitSum(886996));
+console.log("partition", partition("aab"));
