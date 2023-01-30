43cb924de89f48bd1a4cc220aead5a65a8b91d2e
hasibul-99 <hasibul_uap@outlook.com>
2023-01-30 10:30:53 +0600

    1389. Create Target Array in the Given Order

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-function intersection(nums) {
-    var res = nums[0];
-    if (nums.length > 1) {
-        var _loop_1 = function (i) {
-            var ele = nums[i];
-            res = res.filter(function (value) { return ele.includes(value); });
-        };
-        for (var i = 1; i < nums.length; i++) {
-            _loop_1(i);
-        }
+function createTargetArray(nums, index) {
+    var res = [];
+    for (var i = 0; i < index.length; i++) {
+        var idx = index[i];
+        res.splice(idx, 0, nums[i]);
     }
-    return res.sort(function (a, b) { return a - b; });
+    return res;
 }
 ;
-console.log("intersection", intersection([[1, 2, 3], [4, 5, 6]]));
+console.log("createTargetArray", createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
