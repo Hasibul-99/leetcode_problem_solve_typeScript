b076f8cddb15808615f3dd4c5c0cb7ce891e5e4b
hasibul-99 <hasibul_uap@outlook.com>
2023-02-02 23:06:17 +0600

    953. Verifying an Alien Dictionary

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-function createTargetArray(nums, index) {
-    var res = [];
-    for (var i = 0; i < index.length; i++) {
-        var idx = index[i];
-        res.splice(idx, 0, nums[i]);
+function isAlienSorted(words, order) {
+    var map = {};
+    for (var i = 0; i < order.length; i += 1) {
+        map[order[i]] = i;
     }
-    return res;
+    outer: for (var i = 0; i < words.length - 1; i += 1) {
+        var current = words[i];
+        var next = words[i + 1];
+        var minLength = Math.min(current.length, next.length);
+        for (var i_1 = 0; i_1 < minLength; i_1 += 1) {
+            if (current[i_1] === next[i_1])
+                continue;
+            if (map[current[i_1]] < map[next[i_1]])
+                continue outer;
+            if (map[current[i_1]] > map[next[i_1]])
+                return false;
+        }
+        if (current.length > next.length)
+            return false;
+    }
+    return true;
 }
 ;
-console.log("createTargetArray", createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+console.log("isAlienSorted", isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"));
