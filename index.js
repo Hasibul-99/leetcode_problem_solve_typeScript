f1f0ed27f0c2b71f2ba01d80b7da09469c7d40a8
hasibul-99 <hasibul_uap@outlook.com>
2023-02-04 20:37:24 +0600

    6303. Separate the Digits in an Array

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-function isAlienSorted(words, order) {
-    var map = {};
-    for (var i = 0; i < order.length; i += 1) {
-        map[order[i]] = i;
-    }
-    outer: for (var i = 0; i < words.length - 1; i += 1) {
-        var current = words[i];
-        var next = words[i + 1];
-        var minLength = Math.min(current.length, next.length);
-        for (var i_1 = 0; i_1 < minLength; i_1 += 1) {
-            if (current[i_1] === next[i_1])
-                continue;
-            if (map[current[i_1]] < map[next[i_1]])
-                continue outer;
-            if (map[current[i_1]] > map[next[i_1]])
-                return false;
+function separateDigits(nums) {
+    var res = [];
+    for (var i = 0; i < nums.length; i++) {
+        var ele = nums[i];
+        if (ele > 9) {
+            var item = ele.toString();
+            for (var j = 0; j < item.length; j++) {
+                var n = parseInt(item[j]);
+                res.push(n);
+            }
         }
-        if (current.length > next.length)
-            return false;
+        else {
+            res.push(ele);
+        }
     }
-    return true;
+    return res;
 }
 ;
-console.log("isAlienSorted", isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz"));
+console.log("separateDigits", separateDigits([13, 25, 83, 77]));
