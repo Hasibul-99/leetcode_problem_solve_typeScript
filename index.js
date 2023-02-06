550fa6337fd7b118637d597a9a37a09d3d8f9b1b
hasibul-99 <hasibul_uap@outlook.com>
2023-02-06 09:45:48 +0600

    6348. Take Gifts From the Richest Pile

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-function createTargetArray(nums, index) {
+function vowelStrings(words, queries) {
     var res = [];
-    for (var i = 0; i < index.length; i++) {
-        var idx = index[i];
-        res.splice(idx, 0, nums[i]);
+    for (var i = 0; i < queries.length; i++) {
+        var ele = queries[i];
+        var total = 0;
+        for (var j = ele[0]; j <= ele[1]; j++) {
+            console.log("jjj", words[j]);
+            var srt = words[j];
+            // if ((srt.startsWith('a') && srt.endsWith('a')) || ( srt.startsWith('e') && srt.endsWith('e')) || 
+            //     (srt.startsWith('i') && srt.endsWith('i')) || (srt.startsWith('o') && srt.endsWith('o')) || (srt.startsWith('u') && srt.endsWith('u'))) {
+            //         total += 1;
+            //     }
+            if (["a", "e", "i", "o", "u"].includes(srt[srt.length - 1]) && ["a", "e", "i", "o", "u"].includes(srt[0])) {
+                total += 1;
+            }
+        }
+        res.push(total);
     }
     return res;
 }
 ;
-console.log("createTargetArray", createTargetArray([0, 1, 2, 3, 4], [0, 1, 2, 2, 1]));
+console.log("vowelStrings", vowelStrings(["aba", "bcb", "ece", "aa", "e"], [[0, 2], [1, 4], [1, 1]]));
