737ec1f49106e786fca5edea37c519365121fcc1
hasibul-99 <hasibul_uap@outlook.com>
2023-02-15 10:45:53 +0600

    989. Add to Array-Form of Integer

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-function findTheArrayConcVal(nums) {
-    var output = 0;
-    var i = 0;
-    var j = nums.length - 1;
-    while (i <= j) {
-        output += Number(i === j ? nums[i] : "".concat(nums[i]) + "".concat(nums[j]));
-        i++;
-        j--;
-    }
-    ;
-    return output;
+function addToArrayForm(num, k) {
+    // let res: number[] = [],
+    //     remi = 0,
+    //     str = k.toString(),
+    //     len: number = str.length;
+    // for (let i = num.length - 1; i >= 0; i--) {
+    //     const first = num[i];
+    //     let sec = parseInt(str[len - 1]);
+    //     let total = first + (sec || 0) + remi;
+    //     remi = 0;
+    //     if (total <= 9) {
+    //         res.unshift(total);
+    //     } else {
+    //         let item = total.toString().split(""),
+    //             f = parseInt(item[1]),
+    //             s = parseInt(item[0]);[...(BigInt(num.join(""))+BigInt(k)).toString()]
+    //         res.unshift(f);
+    //         remi = s;
+    //     }
+    //     if (i === 0 && remi) {
+    //         res.unshift(remi);
+    //     }
+    //     len -= 1;
+    // }
+    // return res;
+    return (BigInt(num.join("")) + BigInt(k)).toString().split("").map(function (i) { return parseInt(i); });
 }
 ;
-console.log("findTheArrayConcVal", findTheArrayConcVal([7, 52, 2, 4]));
+console.log("addToArrayForm", addToArrayForm([1, 2, 6, 3, 0, 7, 1, 7, 1, 9, 7, 5, 6, 6, 4, 4, 0, 0, 6, 3], 516));
