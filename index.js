206acf1d524873a5536e6965eaea4e1b44538a7c
hasibul-99 <hasibul_uap@outlook.com>
2023-03-12 14:31:18 +0600

    2089. Find Target Indices After Sorting Array

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-function minEatingSpeed(piles, h) {
-    var left = 1, right = Math.max.apply(Math, piles), ans = 0;
-    while (left <= right) {
-        var mid = Math.floor(left + (right - left) / 2);
-        var time = getTime(mid, piles);
-        if (time <= h) {
-            ans = mid;
-            right = mid - 1;
-        }
-        else {
-            left = mid + 1;
-        }
-        console.log({ left: left, right: right, ans: ans, mid: mid });
+function targetIndices(nums, target) {
+    var res = [], len = nums.length, sort = nums.sort(function (a, b) { return a - b; });
+    for (var i = 0; i < len; i++) {
+        if (target === sort[i])
+            res.push(i);
     }
-    ;
-    return ans;
+    return res;
 }
 ;
-var getTime = function (value, piles) {
-    var total = 0;
-    piles.forEach(function (item) {
-        total = total + Math.ceil(item / value);
-    });
-    return total;
-};
-console.log("minEatingSpeed", minEatingSpeed([30, 11, 23, 4, 20], 5));
+console.log("targetIndices", targetIndices([1, 2, 5, 2, 3], 2));
