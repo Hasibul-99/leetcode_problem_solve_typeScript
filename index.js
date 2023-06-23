3c80e5c236629b158df4700efdc8d585c5387bb0
hasibul-99 <hasibul_uap@outlook.com>
2023-06-23 19:38:41 +0600

    189. Rotate Array  solved

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-function largestAltitude(gain) {
-    var res = 0, start = 0;
-    for (var i = 0; i < gain.length; i++) {
-        var ele = start + gain[i];
-        console.log({ start: start, ele: ele }, Math.max(ele, start));
-        res = Math.max(ele, start, res);
-        start = ele;
+function rotate(nums, k) {
+    for (var i = 0; i < k; i++) {
+        var lasNam = nums.pop() || 0;
+        nums.unshift(lasNam);
     }
-    return res > 0 ? res : 0;
 }
 ;
-console.log("largestAltitude", largestAltitude([52, -91, 72]));
+console.log("rotate", rotate([1, 2, 3, 4, 5, 6, 7], 3));
--- index.ts
+++ index.ts
@@ -1,19 +1,8 @@
-function largestAltitude(gain: number[]): number {
-
-    let res = 0,
-        start = 0;
-
-    for (let i = 0; i < gain.length; i++) {
-        const ele = start + gain[i];
-        
-        console.log({start, ele}, Math.max(ele, start));
-        
-        res = Math.max(ele, start, res)
-        start = ele; 
+function rotate(nums: number[], k: number): void {
+    for (let i = 0; i < k; i++) {
+        let lasNam : number = nums.pop() || 0;
+        nums.unshift(lasNam);
     }
-
-
-    return res > 0 ? res : 0;
 };
 
-console.log("largestAltitude", largestAltitude([52,-91,72]));
+console.log("rotate", rotate([1,2,3,4,5,6,7], 3));
