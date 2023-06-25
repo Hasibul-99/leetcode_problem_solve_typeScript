d4aaeef4a2ae2fc090028e36f5ef09d83835eaf1
hasibul-99 <hasibul_uap@outlook.com>
2023-06-25 09:41:35 +0600

    majorityElement

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-function rotate(nums, k) {
-    for (var i = 0; i < k; i++) {
-        var lasNam = nums.pop() || 0;
-        nums.unshift(lasNam);
+function majorityElement(nums) {
+    var res = nums[0], obj = {}, max = 0;
+    for (var i = 0; i < nums.length; i++) {
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1;
+        max = Math.max(obj[nums[i]], max);
+        var key = Object.keys(obj).find(function (key) { return obj[key] === max; }) || 0;
+        res = key * 1;
     }
+    return res;
 }
 ;
-console.log("rotate", rotate([1, 2, 3, 4, 5, 6, 7], 3));
+console.log("majorityElement", majorityElement([2, 2, 1, 1, 1, 2, 2]));
--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
-function rotate(nums: number[], k: number): void {
-    for (let i = 0; i < k; i++) {
-        let lasNam : number = nums.pop() || 0;
-        nums.unshift(lasNam);
+function majorityElement(nums: number[]): number {
+    let res : any = nums[0],
+        obj : any = {},
+        max: number = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        obj[nums[i]] = (obj[nums[i]] || 0) + 1;
+        max = Math.max(obj[nums[i]], max);
+
+        let key : any = Object.keys(obj).find(key => obj[key] === max) || 0;
+        res =  key *1
     }
+    
+    return res;
 };
 
-console.log("rotate", rotate([1,2,3,4,5,6,7], 3));
+console.log("majorityElement", majorityElement([2,2,1,1,1,2,2]));
