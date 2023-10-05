bffe0fce010bb8f92ffadc3fef7808cba74e1493
hasibul-99 <hasibul_uap@outlook.com>
2023-10-05 17:17:45 +0600

    229. Majority Element II

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
-function singleNumber(nums: number[]): number {
+function majorityElement(nums: number[]): number[] {
     let obj: any = {};
 
     for (let i = 0; i < nums.length; i++) {
         obj[nums[i]] = (obj[nums[i]] || 0) + 1;
+    }
+
+    let res: number[] = [];
+    for (const n in obj) {
+        if ((nums.length / 3) < obj[n]) {
+            res.push(parseInt(n))
+        }
     };
 
-    return Number(Object.keys(obj).find(k=>obj[k]===1));
+    return res;
 };
 
-console.log("singleNumber, ", singleNumber([4,1,2,1,2]));
+console.log("majorityElement", majorityElement([3, 2, 3]));
