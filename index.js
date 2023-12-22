493a30833d098e72081f4298309aa06932fe402a
hasibul-99 <hasibul_uap@outlook.com>
2023-12-22 07:00:47 +0600

    657. Robot Return to Origin

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+<<<<<<< HEAD
 function maxWidthOfVerticalArea(points) {
     var xPoint = points.map(function (point) { return point[0]; }).sort(function (a, b) { return a - b; });
     console.log(xPoint);
     var res = 0;
     for (var i = 1; i < xPoint.length; i++) {
         res = Math.max(res, (xPoint[i] - xPoint[i - 1]));
+=======
+function judgeCircle(moves) {
+    var positions = [0, 0];
+    for (var i = 0; i < moves.length; i++) {
+        if (moves[i] === 'U')
+            positions[1] += 1;
+        if (moves[i] === 'D')
+            positions[1] -= 1;
+        if (moves[i] === 'L')
+            positions[0] -= 1;
+        if (moves[i] === 'R')
+            positions[0] += 1;
+>>>>>>> 7df3285 (657. Robot Return to Origin)
     }
     return res;
 }
