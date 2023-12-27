0877beba7eaf9a109965d42a901dc944382d5071
hasibul-99 <hasibul_uap@outlook.com>
2023-12-27 11:16:30 +0600

    1578. Minimum Time to Make Rope Colorful

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function maxWidthOfVerticalArea(points) {
-    var xPoint = points.map(function (point) { return point[0]; }).sort(function (a, b) { return a - b; });
-    console.log(xPoint);
+function minCost(colors, neededTime) {
     var res = 0;
-    for (var i = 1; i < xPoint.length; i++) {
-        res = Math.max(res, (xPoint[i] - xPoint[i - 1]));
+    var num = [];
+    var arr = [neededTime[0]];
+    for (var i = 1; i < colors.length; i++) {
+        if (colors[i - 1] === colors[i]) {
+            arr.push(neededTime[i]);
+        }
+        else {
+            num.push(arr);
+            arr = [neededTime[i]];
+        }
+    }
+    num.push(arr);
+    for (var i = 0; i < num.length; i++) {
+        if (num[i].length > 1) {
+            var maxIndex = num[i].indexOf(Math.max.apply(Math, num[i]));
+            console.log("maxIndex", maxIndex);
+            num[i].splice(maxIndex, 1);
+            res = res + num[i].reduce(function (sum, num) { return sum + num; }, 0);
+        }
     }
     return res;
 }
 ;
-console.log("maxWidthOfVerticalArea", maxWidthOfVerticalArea([[3, 1], [9, 0], [1, 0], [1, 4], [5, 3], [8, 8]]));
+console.log("minCost", minCost("aaaaaaaaaaaaaa", [1, 3, 6, 5, 4, 5, 4, 4, 2, 8, 3, 10, 6, 6]));
--- index.ts
+++ index.ts
@@ -1,15 +1,32 @@
-function maxWidthOfVerticalArea(points: number[][]): number {
-    let xPoint = points.map(point => point[0]).sort((a, b) => a - b);
+function minCost(colors: string, neededTime: number[]): number {
+    let res: number = 0;
+    let num: number[][] = [];
+    let arr: number[] = [neededTime[0]];
 
-    console.log(xPoint);
-
-    let res: number = 0
-    
-    for (let i = 1; i < xPoint.length; i++) {
-        res = Math.max(res, (xPoint[i] - xPoint[i -1]))
+    for (let i = 1; i < colors.length; i++) {
+        if (colors[i - 1] === colors[i]) {
+            arr.push(neededTime[i])
+        } else {
+            num.push(arr);
+            arr = [neededTime[i]];
+        }   
     }
+
+    num.push(arr);
+
+    for (let i = 0; i < num.length; i++) {
+        if (num[i].length > 1) {
+            const maxIndex = num[i].indexOf(Math.max(...num[i]));
+            console.log("maxIndex",maxIndex);
+            
+            num[i].splice(maxIndex, 1);
+
+            res = res + num[i].reduce((sum, num) => sum + num, 0)
+        }
+    }
+    
 
     return res;
 };
 
-console.log("maxWidthOfVerticalArea", maxWidthOfVerticalArea([[3,1],[9,0],[1,0],[1,4],[5,3],[8,8]]));
+console.log("minCost", minCost("aaaaaaaaaaaaaa", [1,3,6,5,4,5,4,4,2,8,3,10,6,6]));
