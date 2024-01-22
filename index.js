83222252a939abac6e4c5ebc95a92ba20c60474b
hasibul-99 <hasibul_uap@outlook.com>
2024-01-22 11:08:28 +0600

    645. Set Mismatch

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-function findWinners(matches) {
-    var obj = {}, res = [[], []];
-    for (var i = 0; i < matches.length; i++) {
-        var win = matches[i][0], loss = matches[i][1];
-        obj[win] = (obj[win] || 0) + 0;
-        obj[loss] = (obj[loss] || 0) + 1;
+function findErrorNums(nums) {
+    var dup = -1, miss = -1;
+    var _loop_1 = function (i) {
+        var count = nums.filter(function (num) { return num === i; }).length;
+        if (count === 2) {
+            dup = i;
+        }
+        else if (count === 0) {
+            miss = i;
+        }
+    };
+    for (var i = 1; i <= nums.length; i++) {
+        _loop_1(i);
     }
-    for (var property in obj) {
-        if (obj[property] === 0)
-            res[0].push(Number(property));
-        if (obj[property] === 1)
-            res[1].push(Number(property));
-    }
-    return res;
+    return [dup, miss];
 }
 ;
-console.log("findWinners", findWinners([[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [4, 9], [10, 4], [10, 9]]));
+console.log("findErrorNums", findErrorNums([1, 1]));
--- index.ts
+++ index.ts
@@ -1,21 +1,18 @@
-function findWinners(matches: number[][]): number[][] {
-    let obj: any = {},
-        res: number[][] = [[], []];
+function findErrorNums(nums: number[]): number[] {
+    let dup = -1,
+        miss = -1;
 
-    for (let i = 0; i < matches.length; i++) {
-        let win = matches[i][0],
-            loss = matches[i][1];
-            
-        obj[win] = (obj[win] || 0) + 0;
-        obj[loss] = (obj[loss] || 0) + 1;
+    for (let i = 1; i <= nums.length; i++) {
+        let count = nums.filter(num => num === i).length;
+
+        if (count === 2) {
+            dup = i;
+        } else if (count === 0) {
+            miss = i;
+        }
     }
 
-    for (const property in obj) {
-        if (obj[property] === 0) res[0].push(Number(property));
-        if (obj[property] === 1) res[1].push(Number(property));
-    }
-
-    return res;
+    return [dup, miss];
 };
 
-console.log("findWinners",findWinners([[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]));
+console.log("findErrorNums", findErrorNums([1,1]));
