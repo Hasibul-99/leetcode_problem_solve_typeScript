8827d7c1b04d2bae41d0ee62459d912730058299
hasibul-99 <hasibul_uap@outlook.com>
2024-02-13 10:24:33 +0600

    2108. Find First Palindromic String in the Array

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-function firstUniqChar(s) {
-    var res = -1;
-    if (s.length === 1)
-        return 0;
-    for (var i = 1; i < s.length + 1; i++) {
-        var arr = s.slice(i, s.length);
-        var arr2 = s.slice(0, i - 1);
-        console.log(s[i - 1], arr.includes(s[i - 1]), arr2.includes(s[i - 1]));
-        if (!arr.includes(s[i - 1]) && !arr2.includes(s[i - 1])) {
-            res = i - 1;
+function firstPalindrome(words) {
+    var res = '';
+    for (var i = 0; i < words.length; i++) {
+        if (words[i].split('').reverse().join('') === words[i]) {
+            res = words[i];
             break;
         }
     }
     return res;
 }
 ;
-console.log("firstUniqChar", firstUniqChar("dddccdbba"));
+console.log("firstPalindrome", firstPalindrome(["abc", "car", "ada", "racecar", "cool"]));
--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
-function firstUniqChar(s: string): number {
-    let res: number = -1;
+function firstPalindrome(words: string[]): string {
+    
+    let res: string = '';
 
-    if (s.length === 1) return 0;
-    for (let i = 1; i < s.length + 1; i++) {
-        let arr = s.slice(i, s.length);
-        let arr2 = s.slice(0, i -1);
-
-        console.log(s[i-1], arr.includes(s[i-1]), arr2.includes(s[i-1]));
-        
-        if (!arr.includes(s[i-1]) && !arr2.includes(s[i-1])) {
-            res = i-1;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].split('').reverse().join('') === words[i]) {
+            res = words[i];
             break;
         }
     }
@@ -17,4 +12,4 @@ function firstUniqChar(s: string): number {
     return res;
 };
 
-console.log("firstUniqChar", firstUniqChar("dddccdbba"));
+console.log("firstPalindrome", firstPalindrome(["abc","car","ada","racecar","cool"]));
