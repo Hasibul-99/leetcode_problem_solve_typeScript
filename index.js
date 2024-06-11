b0ecc221811ee7966723ae8875d6ba54f6b0c543
hasibul-99 <hasibul_uap@outlook.com>
2024-06-11 11:43:11 +0600

    1122. Relative Sort Array

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-function search(nums, target) {
-    var left = 0, right = nums.length - 1, mid;
-    while (left <= right) {
-        mid = Math.floor((left + right) / 2);
-        if (nums[mid] === target) {
-            return mid;
-        }
-        else if (nums[left] <= nums[mid]) {
-            if (target > nums[mid] || target < nums[left]) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
-        }
-        else {
-            if (target < nums[mid] || target > nums[right]) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
-        }
-    }
-    return -1;
+function relativeSortArray(arr1, arr2) {
+    var arr = [];
+    var notMatch = arr1.filter(function (item) {
+        return !arr2.includes(item);
+    }).sort(function (a, b) { return a - b; });
+    arr2.forEach(function (i) {
+        var values = arr1.filter(function (d) { return d === i; });
+        arr = arr.concat(values);
+    });
+    return arr.concat(notMatch);
 }
 ;
-console.log("search", search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log("relativeSortArray", relativeSortArray([2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19], [2, 1, 4, 3, 9, 6]));
