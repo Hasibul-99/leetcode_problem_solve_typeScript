d22cd53dd8a8406c430b2d3edd986ecbea5a6e6e
Hasibul-125 <hasibul.hasan@asthait.com>
2024-08-01 10:44:45 +0600

    2678. Number of Senior Citizens

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function minMovesToSeat(seats, students) {
-    var s = seats.sort(function (a, b) { return a - b; });
-    var st = students.sort(function (a, b) { return a - b; });
-    var res = 0;
-    // Determine the number of iterations based on the minimum of seats and students
-    var iterations = Math.min(s.length, st.length);
-    for (var i = 0; i < iterations; i++) {
-        res += Math.abs(s[i] - st[i]);
+function countSeniors(details) {
+    var res = 0, maxAge = 0;
+    for (var i = 0; i < details.length; i++) {
+        var age = parseInt(details[i].slice(11, 13));
+        if (age > 60) {
+            res++;
+        }
     }
     return res;
 }
 ;
 console.log('====================================');
-console.log(minMovesToSeat([12, 14, 19, 19, 12], [19, 2, 17, 20, 7]));
+console.log("countSeniors", countSeniors(["5612624052M0130", "5378802576M6424", "5447619845F0171", "2941701174O9078"]));
 console.log('====================================');
+// 7868190130 M 75 22
--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
-function minMovesToSeat(seats: number[], students: number[]): number {
-    const s: number[] = seats.sort((a, b) => a - b);
-    const st: number[] = students.sort((a, b) => a - b);
-    let res: number = 0;
-
-    // Determine the number of iterations based on the minimum of seats and students
-    const iterations = Math.min(s.length, st.length);
-
-    for (let i = 0; i < iterations; i++) {
-        res += Math.abs(s[i] - st[i]);
+function countSeniors(details: string[]): number {
+    let res: number = 0,
+        maxAge: number = 0;
+    
+    for (let i = 0; i < details.length; i++) {
+        const age = parseInt(details[i].slice(11, 13));
+        if (age > 60) {
+            res++;
+        }
     }
 
     return res;
 };
 
 console.log('====================================');
-console.log(minMovesToSeat([12,14,19,19,12], [19,2,17,20,7]));
+console.log("countSeniors", countSeniors(["5612624052M0130","5378802576M6424","5447619845F0171","2941701174O9078"]));
 console.log('====================================');
+
+// 7868190130 M 75 22
