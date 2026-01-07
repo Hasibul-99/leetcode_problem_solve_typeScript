d5686ff0521e0f03e10fd36a5fe893ecaf7b9cd8
hasibul-99 <hasibul_uap@outlook.com>
2026-01-07 09:53:23 +0600

    217. Contains Duplicate

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
-function finalPrices(prices: number[]): number[] {
-    for (let i = 0; i < prices.length; i++) {
-        for (let j = i + 1; j < prices.length; j++) {
-            if (prices[j] <= prices[i]) {
-                prices[i] -= prices[j];
-                break;
-            } 
-        }
-    }
-
-    return prices;
+function containsDuplicate(nums: number[]): boolean {
+    return new Set(nums).size !== nums.length
 };
 
-console.log("finalPrices", finalPrices([8,4,6,2,3]));
+console.log("containsDuplicate", containsDuplicate([1,2,3,4]));
