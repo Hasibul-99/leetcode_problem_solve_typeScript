82a894ae60d8532513254e26414b338d37c6b3fa
hasibul-99 <hasibul_uap@outlook.com>
2026-01-11 11:20:36 +0600

    49. Group Anagrams

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function twoSum(nums, target) {
-    var arr = [];
-    for (var i = 0; i < nums.length; i++) {
-        for (var j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                arr.push(i, j);
-            }
+function groupAnagrams(strs) {
+    var map = new Map();
+    for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
+        var str = strs_1[_i];
+        // const sorted = [...str].sort().join('');
+        // if (map.has(sorted)) {
+        //     map.get(sorted)!.push(str);
+        // } else {
+        //     map.set(sorted, [str]);
+        // }
+        var key = str.split('').sort().join('');
+        if (map.has(key)) {
+            map.get(key).push(str);
+        }
+        else {
+            map.set(key, [str]);
         }
     }
-    return arr;
+    return Array.from(map.values());
 }
 ;
-console.log("containsDuplicate", twoSum([2, 7, 11, 15], 9));
+console.log("containsDuplicate", groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
-function twoSum(nums: number[], target: number): number[] {
-    let arr : number[] = [];
+function groupAnagrams(strs: string[]): string[][] {
+    
+    const map: Map<string, string[]> = new Map();
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                arr.push(i, j);
-            }
+    for (const str of strs) {
+        // const sorted = [...str].sort().join('');
+        // if (map.has(sorted)) {
+        //     map.get(sorted)!.push(str);
+        // } else {
+        //     map.set(sorted, [str]);
+        // }
+
+        const key = str.split('').sort().join('');
+        if (map.has(key)) {
+            map.get(key)!.push(str);
+        } else {
+            map.set(key, [str]);
         }
+
     }
 
-    return arr;
+    return Array.from(map.values());
 };
 
-console.log("containsDuplicate", twoSum([2,7,11,15], 9));
+console.log("containsDuplicate", groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
 
